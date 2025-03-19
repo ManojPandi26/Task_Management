3e05947249659f23e0b86cbf55b3d0f0c4bbab13
ManojPandi26 <manojpandi680@gmail.com>
2025-03-19 12:37:08 +0530

    changed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,15 +34,3 @@ function App() {
 
 export default App;
 
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
